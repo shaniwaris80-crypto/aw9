53548adadecf07973364dd2aa49249b38cc0302f
shaniwaris80-crypto <shaniwaris80@gmail.com>
2026-08-15 18:07:03 +0200

    Configurar Firebase nuevo AW9

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB0EQgTmvdD7KnX4UwlGG4dHPROrSb6fUM",
+  authDomain: "aw999-71828.firebaseapp.com",
+  projectId: "aw999-71828",
+  storageBucket: "aw999-71828.firebasestorage.app",
+  messagingSenderId: "401083552527",
+  appId: "1:401083552527:web:80a27997b3e4d2e829e47a",
+  measurementId: "G-768W72H2Q0"
+};
+
+export const AW9 = {
+  companyId: "aw9",
+  ownerUid: "o6rMhcEyUXUstmb7ptClXSv8ejQ2",
+  ownerEmail: "shaniwaris80@gmail.com",
+  sdkVersion: "12.16.0"
+};
